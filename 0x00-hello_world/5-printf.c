e20bddc1a6ab709974a0b94ddfc109e82e32b2e5
Peinah <painepeinah@gmail.com>
2022-03-10 10:07:06 -0800

    Betty documentation correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main: prints with proper grammar, but the outcome is a piece of art
+ * main: prints with proper grammar, 
+ * but the outcome is a piece of art
  * return: prints 0 if exited properly
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }

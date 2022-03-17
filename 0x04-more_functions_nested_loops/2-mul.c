156c40197ed6f2e72fee1deea996b9d70b083dea
Peinah <painepeinah@gmail.com>
2022-03-17 09:44:05 -0700

    first attempt multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - mutiplies two integers
+ * @a : first integer to multiply
+ * @b : second integer
+ * Return: product of integers
+ */
+int mul(int a, int b);
+{
+	int a;
+	int b;
+
+	product = a * b;
+
+	return (product);
+}

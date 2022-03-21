8e94eb3f2f803070d3920daf4d0d21b20c4c4237
Peinah <painepeinah@gmail.com>
2022-03-21 12:07:38 -0700

    correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,7 +6,21 @@
  */
 void print_rev(char *s)
 {
-	char str;
+	int i, j, len;
 
-	_putchar("%s\n", strrev(str));
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
 }

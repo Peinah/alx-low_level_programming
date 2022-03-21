18c6503d5069e4927c32b77eb73d850c55138fac
Peinah <painepeinah@gmail.com>
2022-03-21 09:33:37 -0700

    main.h file and code to reset value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98: resets value to 98
+ * @n: parameter to check
+ */
+void reset_to_98(int *n)
+{
+	int *n;
+
+	n = 402;
+	_putchar("n=%d\n", n);
+	reset_to_98(&n;)
+	_putchar("n=%d\n", n);
+
+	return (0);
+}

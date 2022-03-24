23df10ef848261b8c4556148d4923649c45b0c7a
Peinah <painepeinah@gmail.com>
2022-03-24 13:59:33 -0700

    correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,20 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	j = 0;
 
-	for (j = 0; src[j] != '\0' && j < n; j++)
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
 	{
-		dest[i + j] = src[j];
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 
-	dest[i + j] = '\0';
+	dest[i] = '\0';
+
 	return (dest);
 }

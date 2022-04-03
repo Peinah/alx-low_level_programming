fda4fe6ce5645f17fa045c0f9fb4e3f23406f055
Peinah <painepeinah@gmail.com>
2022-04-03 08:58:45 -0700

    function to print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ * main - outputs name
+ * @argc:contains number  arguments supplied to program
+ * @argv:argument supplied to program
+ * Return:returns 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+		printf("%s\n", *argv);
+
+			return (0);
+}
